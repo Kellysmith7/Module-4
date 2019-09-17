5fe24bca81bb54fea8225e4eba23e25cc2d0ccb4
Kellymsmith7 <kellymsmith7@gmail.com>
2019-09-17 18:07:47 -0500

    ten to thirty fail

--- test/calculate_order_test.py
+++ test/calculate_order_test.py
@@ -3,13 +3,13 @@ from topic3 import calculate_order
 
 
 class MyTestCase(unittest.TestCase):
-    def test_under_ten(self):
-        self.assertEqual(8.65, calculate_order.calculate_order(8, 5, .10))
-        self.assertEqual(8.50, calculate_order.calculate_order(8, 5, .15))
-        self.assertEqual(8.35, calculate_order.calculate_order(8, 5, .20))
-        #self.assertEqual(0, calculate_order(8, 10, .10))
-        #self.assertEqual(0, calculate_order(8, 10, .15))
-        #self.assertEqual(0, calculate_order(8, 10, .20))
+    def test_ten_to_thirty(self):
+        self.assertEqual(14.50, calculate_order.calculate_order(20, 5, .10))
+        self.assertEqual(17.75, calculate_order.calculate_order(20, 5, .15))
+        self.assertEqual(17, calculate_order.calculate_order(20, 5, .20))
+        self.assertEqual(9, calculate_order.calculate_order(20, 10, .10))
+        self.assertEqual(8.5, calculate_order.calculate_order(20, 10, .15))
+        self.assertEqual(8, calculate_order.calculate_order(20, 10, .20))
 
 
 if __name__ == '__main__':
--- topic3/calculate_order.py
+++ topic3/calculate_order.py
@@ -1,12 +1,6 @@
 def calculate_order(price, cash_coupon, percent_coupon):
-    price_after_cash_coupon = price - cash_coupon
-    price = price_after_cash_coupon
-    price_after_percent_discount = price - (price * percent_coupon)
-    price = price_after_percent_discount
-    if price < 10:
-        price = price + 5.95
-    return price
+    pass
 
 
 if __name__ == '__main__':
-    print(calculate_order(8, 5, .15))
+    print(calculate_order(20, 5, .15))
