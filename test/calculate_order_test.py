d433b60d2689e603fe0457135eeb217e132e57b1
Kellymsmith7 <kellymsmith7@gmail.com>
2019-09-17 17:56:40 -0500

    Half Under Ten Test Passed

--- test/calculate_order_test.py
+++ test/calculate_order_test.py
@@ -3,11 +3,10 @@ from topic3 import calculate_order
 
 
 class MyTestCase(unittest.TestCase):
-
     def test_under_ten(self):
-        self.assertEqual(2.70, calculate_order(8, 5, .10))
-        self.assertEqual(2.55, calculate_order(8, 5, .15))
-        self.assertEqual(2.40, calculate_order(8, 5, .20))
+        self.assertEqual(8.65, calculate_order.calculate_order(8, 5, .10))
+        self.assertEqual(8.50, calculate_order.calculate_order(8, 5, .15))
+        self.assertEqual(8.35, calculate_order.calculate_order(8, 5, .20))
         #self.assertEqual(0, calculate_order(8, 10, .10))
         #self.assertEqual(0, calculate_order(8, 10, .15))
         #self.assertEqual(0, calculate_order(8, 10, .20))
--- topic3/calculate_order.py
+++ topic3/calculate_order.py
@@ -1,6 +1,12 @@
-def calculate_order(price, cash_order, percent_coupon):
-    pass
+def calculate_order(price, cash_coupon, percent_coupon):
+    price_after_cash_coupon = price - cash_coupon
+    price = price_after_cash_coupon
+    price_after_percent_discount = price - (price * percent_coupon)
+    price = price_after_percent_discount
+    if price < 10:
+        price = price + 5.95
+    return price
 
 
 if __name__ == '__main__':
-    calculate_order(3, 4, .13)
+    print(calculate_order(8, 5, .15))
