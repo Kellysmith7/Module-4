c61b9b5d02a88a74cd6d83d7163b38b73eedd78c
Kellymsmith7 <kellymsmith7@gmail.com>
2019-09-17 20:38:58 -0500

    Validation Pass

--- test/average_score_test.py
+++ test/average_score_test.py
@@ -4,7 +4,7 @@ from topic4 import average_score_2
 
 class MyTestCase(unittest.TestCase):
     def test_input_validation(self):
-        self.assertEqual(-1, average_score_2.average(-90, 90, 85))
+        self.assertEqual(-1, average_score_2.average(90, 90, -85))
 
 
 if __name__ == '__main__':
