86534d4cb29f4224a2f4c4aa0bd5328ad03c7d91
Kellymsmith7 <kellymsmith7@gmail.com>
2019-09-17 19:36:28 -0500

    over fifty pass

--- test/calculate_order_test.py
+++ test/calculate_order_test.py
@@ -4,12 +4,12 @@ from topic3 import calculate_order
 
 class MyTestCase(unittest.TestCase):
     def test_thirty_to_fifty(self):
-        self.assertEqual(47.95, calculate_order.calculate_order(45, 5, .10))
-        self.assertEqual(45.95, calculate_order.calculate_order(45, 5, .15))
-        self.assertEqual(43.95, calculate_order.calculate_order(45, 5, .20))
-        self.assertEqual(43.45, calculate_order.calculate_order(45, 10, .10))
-        self.assertEqual(44.25, calculate_order.calculate_order(48, 10, .15))
-        self.assertAlmostEqual(42.35, calculate_order.calculate_order(48, 10, .20))
+        self.assertEqual(85.50, calculate_order.calculate_order(100, 5, .10))
+        self.assertEqual(80.75, calculate_order.calculate_order(100, 5, .15))
+        self.assertEqual(76, calculate_order.calculate_order(100, 5, .20))
+        self.assertEqual(81.00, calculate_order.calculate_order(100, 10, .10))
+        self.assertEqual(76.50, calculate_order.calculate_order(100, 10, .15))
+        self.assertEqual(72.00, calculate_order.calculate_order(100, 10, .20))
 
 
 if __name__ == '__main__':
