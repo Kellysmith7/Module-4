e820ad687100b86a60c234d17bb3c7c6c3e1bfdf
Kellymsmith7 <kellymsmith7@gmail.com>
2019-09-17 19:24:04 -0500

    ten to thirty pass

--- test/calculate_order_test.py
+++ test/calculate_order_test.py
@@ -4,12 +4,12 @@ from topic3 import calculate_order
 
 class MyTestCase(unittest.TestCase):
     def test_ten_to_thirty(self):
-        self.assertEqual(14.50, calculate_order.calculate_order(20, 5, .10))
-        self.assertEqual(17.75, calculate_order.calculate_order(20, 5, .15))
-        self.assertEqual(17, calculate_order.calculate_order(20, 5, .20))
-        self.assertEqual(9, calculate_order.calculate_order(20, 10, .10))
-        self.assertEqual(8.5, calculate_order.calculate_order(20, 10, .15))
-        self.assertEqual(8, calculate_order.calculate_order(20, 10, .20))
+        self.assertEqual(25.95, calculate_order.calculate_order(25, 5, .10))
+        self.assertEqual(24.95, calculate_order.calculate_order(25, 5, .15))
+        self.assertEqual(23.95, calculate_order.calculate_order(25, 5, .20))
+        self.assertEqual(21.45, calculate_order.calculate_order(25, 10, .10))
+        self.assertEqual(20.70, calculate_order.calculate_order(25, 10, .15))
+        self.assertEqual(19.95, calculate_order.calculate_order(25, 10, .20))
 
 
 if __name__ == '__main__':
--- topic3/calculate_order.py
+++ topic3/calculate_order.py
@@ -1,6 +1,29 @@
+"""Program to calculate order total
+Author: Kelly Smith
+Program: calculate_order.py
+Last updated: 09/17/2019
+"""
+
+
 def calculate_order(price, cash_coupon, percent_coupon):
-    pass
+    SHIP_UNDER_TEN = 5.95
+    SHIP_TEN_THIRTY = 7.95
+    SHIP_THIRTY_FIFTY = 11.95
+    SHIP_OVER_FIFTY = 0
+    price_after_cash_coupon = price - cash_coupon
+    price = price_after_cash_coupon
+    price_after_percent_discount = price - (price * percent_coupon)
+    price = price_after_percent_discount
+    if price < 10:
+        price = price + SHIP_UNDER_TEN
+    elif 10 < price < 30:
+        price = price + SHIP_TEN_THIRTY
+    elif 30 < price < 50:
+        price = price + SHIP_THIRTY_FIFTY
+    else:
+        price = price + SHIP_OVER_FIFTY
+    return price
 
 
 if __name__ == '__main__':
-    print(calculate_order(20, 5, .15))
+    print("Final price is:  $%5.2f" % calculate_order(25, 10, .15))
