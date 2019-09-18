e237d6085a2ceef2484ad0b4e2c7af616af515cc
Kellymsmith7 <kellymsmith7@gmail.com>
2019-09-17 21:21:17 -0500

    Validation with Value Error raised

--- topic4/validation_with_try.py
+++ topic4/validation_with_try.py
@@ -8,9 +8,13 @@ Program to find the average of three numbers and return negative 1 if a negative
 
 
 def average(x, y, z):
+    if x < 0:
+        raise ValueError
+    elif y < 0:
+        raise ValueError
     scores = [x, y, z]
     return sum(scores) / len(scores)
 
 
 if __name__ == '__main__':
-    print("Average Score: % 5.2f" % average())
+    print("Average Score: % 5.2f" % average(-90, -85, -95))
